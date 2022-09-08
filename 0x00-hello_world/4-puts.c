b37c73671ad8da7b39be8dfc48985c366c83fce4
ezeisaacsunday@gmail.com <ezeisaacsunday@gmail.com>
2022-09-08 14:29:55 +0100

    coding c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(viod)
+{
+	puts("\"Programming is like building a multiligual puzzle"
+	    );
+	return(0);
+
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char acharacter;
+	int ainteger;
+	long along;
+	long long alonglong;
+	float afloat;
+	

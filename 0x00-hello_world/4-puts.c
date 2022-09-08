47d73b20412fb196658f4a8e565649ce70f34ebc
ezeisaacsunday@gmail.com <ezeisaacsunday@gmail.com>
2022-09-08 15:50:54 +0100

    .

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,10 @@
  * Return: Always 0 (Success)
  */
 
-int main (void) 
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle"
+			);
 	return(0);
 
 }

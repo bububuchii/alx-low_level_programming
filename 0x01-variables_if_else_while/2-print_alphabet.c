a52645f40401cf2461293c409a23e730f35e9839
ezeisaacsunday@gmail.com <ezeisaacsunday@gmail.com>
2022-09-09 23:57:03 +0100

    third

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
 /**
-* main - Entry
-*
-* Return: Always 0
-*/
+ * main - Entry
+ *
+ * Return: Always 0
+ */
 
 int main(void)
 {
+
 	char c;
 
-	for(c = 'a'; c <= 'z' ; c++)
-{
-	putchar(c);
-}
-        putchar('\n');
-        return (0);
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }

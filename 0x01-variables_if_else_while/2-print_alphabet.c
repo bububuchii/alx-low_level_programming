19c69269222a855580318da1626b8c560eb93401
ezeisaacsunday@gmail.com <ezeisaacsunday@gmail.com>
2022-09-09 23:43:26 +0100

    third

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry 
+ *
+ * Return: Always
+ */
+
+int main(void)
+{
+	char c;
+
+	for(c = 'a' ; c <= 'z' ; c++)
+{
+	putchar(c);
+}
+       putchar('\n');
+       return (0);
+}
+
+
+

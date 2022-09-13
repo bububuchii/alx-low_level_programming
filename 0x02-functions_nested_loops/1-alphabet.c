187b3ba8946d37789785dfd3c5ca1865ba9ebd93
ezeisaacsunday@gmail.com <ezeisaacsunday@gmail.com>
2022-09-13 15:34:23 +0100

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * Print_alphabet - prints alphabet in lowercase
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int ii;
+	for (ii = 'a'; ii <= 'z'; ii++)
+		_putchar(ii);
+	_putchar('\n');
+}

114fba07017d8a41637198b0baed583c50939689
ezeisaacsunday@gmail.com <ezeisaacsunday@gmail.com>
2022-09-13 16:11:05 +0100

    functions

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - Shows 1 if the output is lowercase or uppercase
+ * Another cases, shows 0
+ *
+ * @c: The character is ASCII mode
+ *
+ * Return: 1 for letters. Returns 0 otherwise
+ */
+int _isalpha
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}

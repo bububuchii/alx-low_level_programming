b55439731795303d01c73f543c30e6271ec93d5b
ezeisaacsunday@gmail.com <ezeisaacsunday@gmail.com>
2022-09-13 16:45:24 +0100

    functions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,14 +10,14 @@ void jack_bauer(void)
 	int hr;
 	int min;
 
-	for (hr = 00:00; hr < 24:00; hr++)
+	for (hr = 0; hr < 24; hr++)
 	{
-		for (min = 00:00; min < 60:00; min++)
+		for (min = 0; min < 60; min++)
 		{
 			_putchar((hr / 10) + '0');
 			_putchar((hr % 10) + '0');
 			_putchar(',');
-			_putchar((min / 10) / '0');
+			_putchar((min / 10) + '0');
 			_putchar((min % 10) + '0');
 			_putchar('\n');
 		}

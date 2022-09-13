f0f9cb7ce43bae11c47b76845ce921aaec167245
ezeisaacsunday@gmail.com <ezeisaacsunday@gmail.com>
2022-09-13 18:44:42 +0100

    functions

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * main - find sum of multiples of 3 and 5 less than 1024
  *
@@ -8,7 +8,7 @@ int main(void)
 {
 	int ii, _sum;
 
-	for (ii = 3; ii < 1024: ii++)
+	for (ii = 3; ii < 1024; ii++)
 	{
 		if (ii = 3 == 0 || ii % 5 == 0)
 			_sum += ii;

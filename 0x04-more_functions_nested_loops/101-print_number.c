6b60f6a18247ae5a84fa2fa6cc4729ab2d7bf539
ezeisaacsunday@gmail.com <ezeisaacsunday@gmail.com>
2022-09-15 14:11:25 +0100

    Loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,8 +6,8 @@ void print(unsigned int n);
  * print_number - print number
  * @var: variable to be printed
  *
- * Created by: Eze-Isaac
- * cc: 15th august, 2022
+ * Created by: Sanctus-Peter
+ * cc: 7th july, 2022
  *
  */
 void print_number(int var)
@@ -18,7 +18,7 @@ void print_number(int var)
 		var = var;
 	}
 
-	printf(var);
+	print(var);
 }
 
 /**
@@ -28,6 +28,6 @@ void print_number(int var)
 void print(unsigned int n)
 {
 	if (n / 10)
-		printf(n / 10);
+		print(n / 10);
 	_putchar('0' + n % 10);
 }

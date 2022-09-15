4d426a168ba6ea8a48215e626880f6e2b925abbe
ezeisaacsunday@gmail.com <ezeisaacsunday@gmail.com>
2022-09-15 11:57:41 +0100

    Loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: Variable text
+ * Return: Always 0.
+ */
+int _isupper(int 0);
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

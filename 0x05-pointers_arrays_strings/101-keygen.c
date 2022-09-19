02c2636a4300a6f1ba381f5eaee398fd7ec225bf
ezeisaacsunday@gmail.com <ezeisaacsunday@gmail.com>
2022-09-19 19:06:10 +0100

    Arrays, Strings, and Pointers

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -18,7 +18,7 @@ int main(void)
         for (count = 0, total = 2772; total > 122; count++)
         {
                 myrand = (rand() % 125) + 1;
-                printf("%c", myran);
+                printf("%c", myrand);
                 total -= myrand;
         }
         printf("%c", total);

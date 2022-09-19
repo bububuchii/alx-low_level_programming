137512edbb550f9f8d22fa37a50443c74e424f71
ezeisaacsunday@gmail.com <ezeisaacsunday@gmail.com>
2022-09-19 15:58:00 +0100

    Arrays, Strings, and Pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * swap_int - swap the values of the two integers.
+ * @a: value to be evaluate.
+ * @b: value to be evaluate.
+ * Return: not.
+ */
+void sawo_int(int *a, int *b)
+{
+	int n = *a;
+	*a = *b;
+	*b = n;
+}

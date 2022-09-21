e8b9213ebf25e8ab27a14068895a94b079102656
ezeisaacsunday@gmail.com <ezeisaacsunday@gmail.com>
2022-09-21 18:52:25 +0100

    Pointers, arrays, strings part 2

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,18 +8,18 @@
  */
 void print_number(int n)
 {
-	unsigned int holder = 1, dumy = n, x;
+	unsigned int holder = 1, dummy = n, x;
 
 	if (n < 0)
 	{
 		_putchar('-');
 		n =n * -1;
-		dumy = n;
+		dummy = n;
 	}
 
-	while (dumy >0)
+	while (dumy > 9)
 	{
-		dumy = dumy / 10;
+		dummy = dummy / 10;
 		holder = holder * 10;
 	}
 

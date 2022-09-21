d9177dac48fcb35955ed8f3d1cde7e60050b370b
ezeisaacsunday@gmail.com <ezeisaacsunday@gmail.com>
2022-09-21 18:32:31 +0100

    Pointers, arrays, strings part 2

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,10 +10,11 @@ char *string_toupper(char *str)
 {
 	int i;
 
-	for (i = o; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] -= 32;
 	}
+
 	return (str);
 }

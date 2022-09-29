49e89bdbe7dcb14ab4098f1d58fdd406b3b08ac9
ezeisaacsunday@gmail.com <ezeisaacsunday@gmail.com>
2022-09-29 14:59:51 +0100

    Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,7 +14,7 @@ int wildcmp(char *s1, char *s2)
 
 	if (*s2 == '*' && *(s2 + 1) == '\0')
 		return (1);
-	else if (*s2 == '*' && (s2 + 1) == '*')
+	else if (*s2 == '*' && *(s2 + 1) == '*')
 		return (wildcmp(s1, s2 + 1));
 	else if (*s2 == '*')
 	{
@@ -25,7 +25,7 @@ int wildcmp(char *s1, char *s2)
 	}
 	else if (*s1 != *s2)
 		return (0);
-	if (*s1 == '\0');
+	if (*s1 == '\0')
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 

862622c111147a4bf2fd125fb0027d77f903b2f3
ezeisaacsunday@gmail.com <ezeisaacsunday@gmail.com>
2022-10-03 14:28:09 +0100

    Lib

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,31 +7,16 @@
  */
 int _atoi(char *s)
 {
-	int i, value, sign;
+	int sign = 1;
+	unsigned int num = 0;
 
-	char c;
-
-	i = 0;
-	value = 0;
-	sign = 1;
-	while (s[i] != '\0')
-	{
-		c = s[i];
-		if (c == '-')
+	do {
+		if (*s == '-')
 			sign *= -1;
-		else if (c >= '0' && c <= '9')
-		{
-			if (value < 0)
-	value = value * 10 - (c - '0');
-			else
-				value = (c - '0') * -1;
-/* Make sure the next char iis a number */
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-		}
-		i++;
-	}
-	if (sign < 0)
-		value *= -1;
-	return (value);
+		else if (*s >= '0' && *s <= '9')
+			num = num * 10 + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+	return (num * sign);
 }
